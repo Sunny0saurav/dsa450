9b105a2a14e66d6c7c7aaebed702ea4a12b63a1e
Sunny0saurav <93779290+Sunny0saurav@users.noreply.github.com>
2022-04-28 22:29:35 +0530

    Add files via upload
    complete

--- Q1.java
+++ Q1.java
@@ -8,13 +8,18 @@ public class Q1 {
             start++;
             end--;
 
+        }}
+
+        static void printArray(int[] arr,int size) {
+            for(int i=0;i<arr.length;i++)
+                System.out.print(arr[i]+" ");
+            System.out.println();
         }
-        static void printArray(int[] arr,)
-    }
     public static void main(String args[]){
-        int[] arr={2,3,4,5,6,7,10};
-
-
+        int[] arr={1,2,3,4,5,6};
+        printArray(arr,6);
+        reverseArray(arr,0,5);
+        printArray(arr,6);
 
 
     }
