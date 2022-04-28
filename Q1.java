07eb3524d4d7de40b639f3ca16050d1147c11f80
Sunny0saurav <93779290+Sunny0saurav@users.noreply.github.com>
2022-04-28 21:56:05 +0530

    first commit

---
+++ Q1.java
@@ -0,0 +1,21 @@
+public class Q1 {
+    static void reverseArray(int[] arr,int start,int end){
+        int temp;
+        while(start<end){
+            temp=arr[start];
+            arr[start]=arr[end];
+            arr[end]=temp;
+            start++;
+            end--;
+
+        }
+        static void printArray(int[] arr,)
+    }
+    public static void main(String args[]){
+        int[] arr={2,3,4,5,6,7,8};
+
+
+
+
+    }
+}
