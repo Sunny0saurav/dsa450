66e27e87f89ec837decd0fe1e2415a75700fc08c
Sunny0saurav <93779290+Sunny0saurav@users.noreply.github.com>
2022-04-28 22:03:31 +0530

    8 to 9

--- Q1.java
+++ Q1.java
@@ -12,7 +12,7 @@ public class Q1 {
         static void printArray(int[] arr,)
     }
     public static void main(String args[]){
-        int[] arr={2,3,4,5,6,7,8};
+        int[] arr={2,3,4,5,6,7,9};
 
 
 
