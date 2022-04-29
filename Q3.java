3672cab37434893c9ce71ef8525e4c6adc2f2dc0
Sunny0saurav <93779290+Sunny0saurav@users.noreply.github.com>
2022-04-29 20:39:57 +0530

    Add files via upload

---
+++ Q3.java
@@ -0,0 +1,28 @@
+public class Q3 {
+    static void maxMin_kth(int[]arr,int size) {
+        int max = arr[0], min = arr[0], kmax = 0, kmin = 0;
+        for (int i = 0; i<size;i++){
+            if(arr[i]>max){
+                max=arr[i];
+                kmax=i+1;
+            }
+            if(arr[i]<min){
+                min=arr[i];
+                kmin=i+1;
+            }
+        }
+        System.out.println(kmax+"th element has max value:-"+max);
+        System.out.println(kmin+"th element has min value:-"+min);
+    }
+    static void printArray(int arr[],int size){
+        for(int i=0;i<size;i++){
+            System.out.print(arr[i]+",");
+        }
+    }
+    public static void main(String args[]){
+        int[] arr={1000,11,445,1,330,3000};
+        maxMin_kth(arr,6);
+        printArray(arr,6);
+    }
+
+}
