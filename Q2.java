755126cc7b0725f694a8c1d2aca2353a603b5b33
Sunny0saurav <93779290+Sunny0saurav@users.noreply.github.com>
2022-04-29 20:15:34 +0530

    Add files via upload

---
+++ Q2.java
@@ -0,0 +1,28 @@
+import javax.swing.*;
+
+public class Q2 {
+    static void maxMinArray(int[] arr,int size){
+        int max=arr[0],min=arr[0];
+        for(int i=0;i<size;i++){
+            if(arr[i]>max){
+                max=arr[i];
+            }
+            if(arr[i]<min){
+                min=arr[i];
+            }
+
+        }
+        System.out.println(max);
+        System.out.println(min);
+    }
+    static void PrintArray(int[] arr,int size){
+        for(int i=0;i<size;i++){
+            System.out.print(arr[i]+" ");
+        }
+    }
+    public static void main(String args[]){
+        int[] arr={1000,11,445,1,330,3000};
+        maxMinArray(arr,6);
+        PrintArray(arr,6);
+    }
+}
